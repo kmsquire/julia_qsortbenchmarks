3dd4e4099dc476637c4f50065c4eec5910c1becf
illerucis <rob.sicurelli@gmail.com>
2013-10-21 14:19:01 -0400

    partitioned some files

--- qsorts.jl
+++ qsorts.jl
@@ -1,164 +1,166 @@
-const SMALL_THRESHOLD = 20
-const SMALL_THRESHOLD_STDLIB = 20
+require("qsort_c!.jl")
+require("qsort_stdlib.jl")
 
-# insertion sort
-function isort(v, lo=1, hi=length(v))
-    @inbounds for i = lo+1:hi
-        j = i
-        x = v[i]
-        while j > lo
-            if isless(x, v[j-1])
-                v[j] = v[j-1]
-                j -= 1
-                continue
-            end
-            break
-        end
-        v[j] = x
-    end
-end
+# const SMALL_THRESHOLD = 20
+# const SMALL_THRESHOLD_STDLIB = 20
 
-# returns median of three values in array v
-function getmedpiv(v, a, b, c)
-    if isless(v[a], v[b])
-        if isless(v[b], v[c])
-            return b
-        elseif isless(v[a], v[c])
-            return c
-        else
-            return a
-        end
-    else
-        if isless(v[c], v[b])
-            return b
-        elseif isless(v[c], v[a])
-            return c
-        else
-            return a
-        end
-    end
-end
+# function isort(v, lo=1, hi=length(v))
+#     @inbounds for i = lo+1:hi
+#         j = i
+#         x = v[i]
+#         while j > lo
+#             if isless(x, v[j-1])
+#                 v[j] = v[j-1]
+#                 j -= 1
+#                 continue
+#             end
+#             break
+#         end
+#         v[j] = x
+#     end
+# end
 
-# fisher-yates random shuffle
-function fyshuffle(v)
-    N = length(v);
-    for i = length(v):0
-        j = rand(1:N-1);
-        v[i], v[j] = v[j], v[i];
-        N -= 1;
-    end
-end
+# function getmedpiv(v, a, b, c)
+#     if isless(v[a], v[b])
+#         if isless(v[b], v[c])
+#             return b
+#         elseif isless(v[a], v[c])
+#             return c
+#         else
+#             return a
+#         end
+#     else
+#         if isless(v[c], v[b])
+#             return b
+#         elseif isless(v[c], v[a])
+#             return c
+#         else
+#             return a
+#         end
+#     end
+# end
 
-# canonical quicksort + fisher-yates shuffle
-function qsort_c_fy!(v, lo=1, hi=length(v))
-    fyshuffle(v)
-    qsort_c!(v)
-end
+# function fyshuffle(v)
+#     @inbounds begin
+#         N = length(v);
+#         for i = length(v):0
+#             j = rand(1:N-1);
+#             v[i], v[j] = v[j], v[i];
+#             N -= 1;
+#         end
+#     end
+# end
 
-# canonical quicksort + fisher-yates shuffle + median of 3 pivot
-function qsort_c_fy_mp!(v, lo=1, hi=length(v))
-    fyshuffle(v)
-    qsort_c_mp!(v)
-end
 
-# canonical quicksort + median of 3 pivot
-function qsort_c_mp!(v, lo=1, hi=length(v))
-    hi <= lo && return;
-    hi-lo <= SMALL_THRESHOLD && return isort(v, lo, hi)
-    mi = (lo+hi)>>>1
-    if v[lo] > v[mi]
-        v[lo], v[mi] = v[mi], v[lo];
-    end
-    if v[lo] > v[hi] 
-        v[lo], v[hi] = v[hi], v[lo];
-    end
-    if v[mi] > v[hi]
-        v[mi], v[hi] = v[hi], v[mi];
-    end
-    v[mi], v[lo] = v[lo], v[mi]
-    i, j = lo, hi+1;
-    pivot = v[lo]
-    while true;
-        i += 1;
-        while isless(v[i], pivot);
-            i == hi && break;
-            i += 1;
-        end
-        j -= 1;
-        while isless(pivot, v[j]);
-            j -= 1;
-        end 
-        i >= j && break;
-        v[i], v[j] = v[j], v[i];
-    end
-    v[j], v[lo] = v[lo], v[j];
-    qsort_c_mp!(v, lo, j-1);
-    qsort_c_mp!(v, j+1, hi);
-    return v;
-end
+# function qsort_c_fy!(v, lo=1, hi=length(v))
+#     fyshuffle(v)
+#     qsort_c!(v)
+# end
 
-# canonical quicksort
-function qsort_c!(v, lo=1, hi=length(v))        
-    hi <= lo && return;
-    hi-lo <= SMALL_THRESHOLD && return isort(v, lo, hi)
-    i, j = lo, hi+1;
-    pivot = v[lo]
-    while true;
-        i += 1;
-        while isless(v[i], pivot);
-            i == hi && break;
-            i += 1;
-        end
-        j -= 1;
-        while isless(pivot, v[j]);
-            j -= 1;                
-        end 
-        i >= j && break;
-        v[j], v[i] = v[i], v[j];
-    end
-    v[j], v[lo] = v[lo], v[j];
-    qsort_c!(v, lo, j-1);
-    qsort_c!(v, j+1, hi);
-    return v;        
-end
 
-# 3 way quicksort
-function qsort_3way!(v, lo=1, hi=length(v))
-    hi <= lo && return;
-    lt = lo; gt = hi; i = lo;
-    piv = v[lo];
-    while i <= gt
-        elem = v[i];
-        if isless(elem, piv)
-            v[i], v[lt] = v[lt], v[i];
-            lt+=1; i+=1;
-        elseif isless(piv, elem)
-            v[i], v[gt] = v[gt], v[i];
-            gt-=1;
-        else i+=1; end
-    end
-    qsort_3way!(v, lo, lt-1)
-    qsort_3way!(v, gt+1, hi)
-end
+# function qsort_c_fy_mp!(v, lo=1, hi=length(v))
+#     fyshuffle(v)
+#     qsort_c_mp!(v)
+# end
 
-# quicksort as written for the standard library
-function qsort_stdlib!(v, lo=1, hi=length(v))
-    while lo < hi
-        hi-lo <= SMALL_THRESHOLD_STDLIB && return isort(v, lo, hi)
-        pivot = v[(lo+hi)>>>1]
-        i, j = lo, hi
-        while true;
-            while isless(v[i], pivot); i += 1; end
-            while isless(pivot, v[j]); j -= 1; end
-            i <= j || break;
-            v[i], v[j] = v[j], v[i]
-            i += 1; j -= 1;
-        end
-        lo < j && qsort_stdlib!(v, lo, j)
-        lo = i
-    end
-    return v;
-end
+
+# function qsort_c_mp!(v, lo=1, hi=length(v))
+#     hi <= lo && return;
+#     hi-lo <= SMALL_THRESHOLD && return isort(v, lo, hi)
+#     mi = (lo+hi)>>>1
+#     if v[lo] > v[mi]
+#         v[lo], v[mi] = v[mi], v[lo];
+#     end
+#     if v[lo] > v[hi] 
+#         v[lo], v[hi] = v[hi], v[lo];
+#     end
+#     if v[mi] > v[hi]
+#         v[mi], v[hi] = v[hi], v[mi];
+#     end
+#     v[mi], v[lo] = v[lo], v[mi]
+#     i, j = lo, hi+1;
+#     pivot = v[lo]
+#     while true;
+#         i += 1;
+#         while isless(v[i], pivot);
+#             i == hi && break;
+#             i += 1;
+#         end
+#         j -= 1;
+#         while isless(pivot, v[j]);
+#             j -= 1;
+#         end 
+#         i >= j && break;
+#         v[i], v[j] = v[j], v[i];
+#     end
+#     v[j], v[lo] = v[lo], v[j];
+#     qsort_c_mp!(v, lo, j-1);
+#     qsort_c_mp!(v, j+1, hi);
+#     return v;
+# end
+
+
+# function qsort_c!(v, lo=1, hi=length(v))        
+#     @inbounds begin
+#         hi <= lo && return;
+#         hi-lo <= SMALL_THRESHOLD && return isort(v, lo, hi)
+#         i, j = lo, hi+1;
+#         pivot = v[lo]
+#         while true;
+#             i += 1;
+#             while isless(v[i], pivot);
+#                 i == hi && break;
+#                 i += 1;
+#             end
+#             j -= 1;
+#             while isless(pivot, v[j]);
+#                 j -= 1;                
+#             end 
+#             i >= j && break;
+#             v[j], v[i] = v[i], v[j];
+#         end
+#         v[j], v[lo] = v[lo], v[j];
+#         qsort_c!(v, lo, j-1);
+#         qsort_c!(v, j+1, hi);
+#         return v;
+#     end
+# end
+
+# function qsort_3way!(v, lo=1, hi=length(v))
+#     hi <= lo && return;
+#     lt = lo; gt = hi; i = lo;
+#     piv = v[lo];
+#     while i <= gt
+#         elem = v[i];
+#         if isless(elem, piv)
+#             v[i], v[lt] = v[lt], v[i];
+#             lt+=1; i+=1;
+#         elseif isless(piv, elem)
+#             v[i], v[gt] = v[gt], v[i];
+#             gt-=1;
+#         else i+=1; end
+#     end
+#     qsort_3way!(v, lo, lt-1)
+#     qsort_3way!(v, gt+1, hi)
+# end
+
+# function qsort_stdlib!(v, lo=1, hi=length(v))
+#     @inbounds while lo < hi
+#         hi-lo <= SMALL_THRESHOLD_STDLIB && return isort(v, lo, hi)
+#         pivot = v[(lo+hi)>>>1]
+#         i, j = lo, hi
+#         while true;
+#             while isless(v[i], pivot); i += 1; end
+#             while isless(pivot, v[j]); j -= 1; end
+#             i <= j || break;
+#             v[i], v[j] = v[j], v[i]
+#             i += 1; j -= 1;
+#         end
+#         lo < j && qsort_stdlib!(v, lo, j)
+#         lo = i
+#     end
+#     return v;
+# end
 
 # ------------------------------------------------------- #
 # ------------------- Benchmarking ---------------------- #
@@ -180,7 +182,7 @@ toq()
 
 # ------------------- Benchmarking --------------------- #
 
-numsims = 10^3
+numsims = 10^2
 
 # arrays to store the sorting times
 
@@ -205,5 +207,5 @@ qs_test_times = Array(Float64, numsims)
 
 end
 
-@show(median(qs_test_times./qs_stdlib_times))
-@show(mean(qs_test_times./qs_stdlib_times))
+# @show(median(qs_test_times./qs_stdlib_times))
+# @show(mean(qs_test_times./qs_stdlib_times))
